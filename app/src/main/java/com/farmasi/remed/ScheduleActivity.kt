313d8664e5a3fcca5cd28fe88513f265258b0994
Muhammad Sadewa Wicaksana W <wicaksana_irfan@yahoo.com>
2019-07-22 10:08:02 +0700

    Penambahan Activity Schedule

---
+++ app/src/main/java/com/farmasi/remed/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.farmasi.remed
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
